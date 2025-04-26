3332475c5bea326c04819372f62156672a1a9606
NaviSiuTu <isantisteban@unal.edu.co>
2025-04-26 17:34:54 -0500

    Estructuración del problema "Volumen de un solido I", definición de la función a manejar al igual que sus variables.
    Diseño de "interfaz" (para terminar)
    
    Explicación de cada parte de la función (por terminar)

---
+++ EJERCICIOS_PAG_42/VolumenSolido.py
@@ -0,0 +1,16 @@
+import math #Lo invocamos para poder utilizar más adelante el numero "pi" ya que se utiliza en la fórmula de volumen
+
+def Volumen(r1, h, r2): #Función que define variables como el radio 1, la altura y el radio 2 (r1, h y r2 respectivamente)
+
+    print ("Vamos a calcular el volumen de un solido")
+    print ("________________________________________") #Este es un objeto de estética que funciona con el objetivo de separar secciones
+
+#Iniciaremos con el volumen del primer objeto (Esfera) teniendo en cuenta la fórmula para hallar su volumen
+
+#Luego hallamos el volumen del segundo objeto (Cono) teniendo en cuenta la fórmula para hallar su volumen
+
+
+    print (f"El resultado final es {} ¡Qué interesante!")
+    print ("_________________________________________") #Este es un objeto de estética que funciona con el objetivo de separar secciones
+
+Volumen(3, 9/2, 4) #Función, definiendo los valores a utilizar en r1, h y r2
