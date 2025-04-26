684aecb4208ebf03538f1a7525d99e5bc1903232
NaviSiuTu <isantisteban@unal.edu.co>
2025-04-26 15:20:51 -0500

    COMENTARIOS en las lineas 9,10 y 11 del ejercicio "Coulumbinator.py"

--- EJERCICIOS_PAG_42/Coulumbinator.py
+++ EJERCICIOS_PAG_42/Coulumbinator.py
@@ -3,12 +3,12 @@
 
 def coulumbinator():
 
-    print ("Bienvenido a la calculadora de la ley de Coulumb\nencontrarás el valor de la fuerza entre dos cargas") #Bienvenida al programa
+    print ("Bienvenido a la calculadora de la ley de Coulumb\nencontrarás el valor de la fuerza entre dos cargas eléctricas") #Bienvenida al programa
     print ("_________________________________________________________________________________") #Este es un objeto de estética que funciona con el objetivo de separar secciones
 
-    Q1 = float(input("Escribe el valor de la primera carga"))
-    Q2 = float(input("Escribe el valor de la segunda carga "))
-    R = float (input("Escribe el valor de la distancia que hay entre estás cargas "))
+    Q1 = float(input("Escribe el valor de la primera carga")) #Se define la variable Q1 que representa el valor de la carga 1
+    Q2 = float(input("Escribe el valor de la segunda carga ")) #Se define la variable Q2 que representa el valor de la carga 2
+    R = float (input("Escribe el valor de la distancia que hay entre estás cargas ")) #Se define la variable R que representa el valor de la distancia entre las cargas
 
     
     print ("_________________________________________________________________________________") #Este es un objeto de estética que funciona con el objetivo de separar secciones
