116490dbfa2ccb90670bcc87f1be8dd396ffee8e
NaviSiuTu <isantisteban@unal.edu.co>
2025-04-27 20:21:52 -0500

    Add de variable "area_vagon2", además de otros detalles estéticos del ejercicio

--- EJERCICIOS_PAG_42/AreaLateral_I.py
+++ EJERCICIOS_PAG_42/AreaLateral_I.py
@@ -1,7 +1,8 @@
 import math #Se utiliza para poder invocar mas tarde el numero pi que hará parte de las fórmulas para solucionar el problema
 
 def AreaVagon():
-
+    
+    
     Radio = float(input("¿Cuál es el radio de las ruedas? ")) #Al ser un problema donde las ruedas del vagón son iguales entre si, significa que este radio es universal para todas (en este problema)
     Base = float (input("¿Cuál es el la base del vagón? ")) #Se pregunta al usuario a través de la consola la base del vagón
     Altura = float (input("¿Cuál es el la altura del vagón? ")) #Se pregunta al usuario a través de la consola la altura del vagón
--- EJERCICIOS_PAG_42/AreaLateral_II.py
+++ EJERCICIOS_PAG_42/AreaLateral_II.py
@@ -17,21 +17,28 @@ def AreaCarro():
     r2 = float(input("¿Cuál es el radio de la segunda rueda? "))
     
     print("---- Datos del vagon ----")
-    b1 = float(input("¿Cuál es la base del vagon? "))
-    h1 = float(input("¿Cuál es la altura del vagon? "))
+    b1 = float(input("¿Cuál es la base del vagon 1? "))
+    h1 = float(input("¿Cuál es la altura del vagon 1? "))
+    b2 = float(input("¿Cuál es la base del vagon 2? "))
+    h2 = float(input("¿Cuál es la altura del vagon 2? "))
+    print("---- Resultados ----")
     
    
 
     # Calculamos áreas usando las funciones
+    print ("---- Area de la rueda 1 ----")
     area_rueda1 = AreaCirculo(r1)
+    print ("---- Area de la rueda 2 ----")
     area_rueda2 = AreaCirculo(r2)
+    print ("---- Area del vagon 1 (grande) ----")
     area_vagon1 = AreaRectangulo(b1, h1)
-    
-
+    print ("---- Area del vagon 2 (pequeño) ----")
+    area_vagon2 = AreaRectangulo(b2, h2)
    
-    Area_total = area_rueda1 + area_rueda2 + area_vagon1   # Sumatoria de áreas
+    Area_total = area_rueda1 + area_rueda2 + area_vagon1 + area_vagon2  # Sumatoria de áreas
    
-    print (f"El area final del carro es {Area_total:.2f} m/2 WOOOOOOW")   
+    print (f"El area final del carro es {Area_total:.2f} m/2 WOOOOOOW")  
+    print ("---- Fin del programa ----")
     
     return (AreaCarro()) #Retorna al usuario la función incial
 
